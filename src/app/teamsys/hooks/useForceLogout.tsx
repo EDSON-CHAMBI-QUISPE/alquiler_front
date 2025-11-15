6f14019a319fa6e7a601089d1a8b5a39b190ab79
EDSON-CHAMBI-QUISPE <202103868@est.umss.edu>
2025-11-14 22:35:20 -0400

    ultimo deploy

---
+++ src/app/teamsys/hooks/useForceLogout.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
+import { getSocket } from "../realtime/socketClient";
+import { cerrarSesion } from "../services/UserService";
+
+export function useForceLogout(userId: string | null) {
+    console.log("🔥 useForceLogout EJECUTADO. userId =", userId);
+
+    useEffect(() => {
+    console.log("🔍 [useForceLogout] Hook montado. userId =", userId);
+    if (!userId) return;
+    console.log("🔥 getSocket fue llamado");
+
+    const socket = getSocket();
+
+    const onConnect = () => {
+        console.log("🔌 [Socket] Conectado con ID:", socket.id);
+      console.log("📡 [Socket] Emitting auth for user:", userId);
+      socket.emit("auth", userId);
+    };
+
+    const onForceLogout = () => {
+        console.log("🚨 [Socket] force-logout recibido desde el servidor!");
+      cerrarSesion();
+    };
+
+    // Si ya está conectado, autenticamos directamente
+    if (socket.connected) {
+      console.log("⚡ [Socket] Ya estaba conectado antes");
+      onConnect();
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("force-logout", onForceLogout);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("force-logout", onForceLogout);
+    };
+  }, [userId]);
+}
